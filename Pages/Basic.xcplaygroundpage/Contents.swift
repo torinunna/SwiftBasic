a1a9f92d8e93295099d8a6c779dc00d7b992d93e
torinunna <torinunna9588@gmail.com>
2023-06-25 15:30:16 +0900

    Basic

---
+++ Pages/Basic.xcplaygroundpage/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+// Variable: 변수
+var str = "Hello, playground"
+print(str)
+str = "Hello, Swift"
+print(str)
+
+
+// Type - Integer
+var age = 30
+var money = 2_000_000
+print(age)
+print(money)
+
+// Type - String
+var name = "Jason"
+print(name)
+
+// Type - Double
+var gpa = 4.5
+print(gpa)
+
+// Type - Boolean
+var isEnabled = false
+print(isEnabled)
+
+
+// String Interpolation
+var height = 180
+var text = "His heigth is \(height)"
+print(text)
+
+
+// Constant: 상수 -> 할당 값 변경 X
+let dj = "Koo"
+print(dj)
+
+
+// Type Annotation: type 명시
+var days: Int = 5
+var userName: String = "Jake"
+
+// Type Inference: type 추론
+var weeks = 10
+var id = "qwer123"
+
